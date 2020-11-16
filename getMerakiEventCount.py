08f23875299d9255176a726dc36c3242e93b86da
Gabriel <gabriel.toubeau@gmail.com>
2020-11-16 08:53:48 +0100

    Optimizing with https://community.meraki.com/t5/Developers-APIs/Python-amp-Dashboard-API-Simple-event-log-counter/m-p/102831#M4149 suggestions (Greenberet)

--- getMerakiEventCount.py
+++ getMerakiEventCount.py
@@ -8,53 +8,57 @@ import sys
 import datetime
 
 
-def getorganisations(apikey):
-    session = meraki.DashboardAPI(api_key=apikey, print_console=False, output_log=False, suppress_logging=True)
-    org = session.organizations.getOrganizations()
+def session(apikey):
+    s = meraki.DashboardAPI(api_key=apikey, print_console=False, output_log=False, suppress_logging=True)
+    return s
+
+
+def getorganisations(s):
+    org = s.organizations.getOrganizations()
     return org
 
 
-def getnetworks(apikey, org):
-    session = meraki.DashboardAPI(api_key=apikey, print_console=False, output_log=False, suppress_logging=True)
-    net = session.organizations.getOrganizationNetworks(organizationId=org)
+def getnetworks(s, org):
+    net = s.organizations.getOrganizationNetworks(organizationId=org)
     return net
 
 
-def getnbevents(apikey, netID, product, event_type, timestamp):
-    session = meraki.DashboardAPI(api_key=apikey, print_console=False, output_log=False, suppress_logging=True)
-    log = session.networks.getNetworkEvents(networkId=netID, productType=product, includedEventTypes=event_type,
-                                            startingAfter=timestamp, perPage="1000")
-    counter = 0
-    for event in log['events']:
-        counter = counter + 1
-    return counter
+def getnbevents(s, netID, product, event_type, timestamp):
+    log = s.networks.getNetworkEvents(networkId=netID, productType=product, includedEventTypes=event_type,
+                                      startingAfter=timestamp, perPage="1000")
+    return len(log['events'])
 
 
 def main():
     # set arguments
     var_parser = argparse.ArgumentParser()
-    var_parser.add_argument("-k", "--key", help="Set up Meraki API Key or set into Env variable", type=str, required=False)
+    var_parser.add_argument("-k", "--key", help="Set up Meraki API Key or set into Env variable", type=str,
+                            required=False)
     var_parser.add_argument("-o", "--org", help="Set Org ID", type=str, required=False)
     var_parser.add_argument("-n", "--net", help="Set Network ID", type=str, required=False)
-    var_parser.add_argument("-s", "--span", help="Set Timespan in days", type=int, required=False)
+    var_parser.add_argument("-s", "--span", help="Set Timespan in days", type=int, required=False, default=1)
     var_parser.add_argument("-t", "--type", help="Set Event Type", type=str, required=False)
-    var_parser.add_argument("-p", "--product", help="Set Product", type=str, required=True)
+    var_parser.add_argument("-p", "--product", help="Set Product", type=str, required=True,
+                            choices=['wireless', 'appliance', 'switch', 'systemsManager', 'camera', 'cellularGateway',
+                                     'environmental'])
 
     # Set parsed arguments in variables
-    variables = var_parser.parse_args()
-    org = variables.org
-    net = variables.net
-    span = variables.span
-    eventtype = variables.type
-    prod = variables.product
+    try:
+        variables = var_parser.parse_args()
+        org = variables.org
+        net = variables.net
+        span = variables.span
+        eventtype = variables.type
+        prod = variables.product
+    except SystemExit:
+        return
+    except:
+        print("Could not parse arguments")
+        var_parser.print_help()
+        return
     # Timestamp generation
     timenow = datetime.datetime.now()
-    if not span:
-        timedelta = timenow - datetime.timedelta(days=1)
-        print("Using default timespan : 1 day")
-    else:
-        timedelta = timenow - datetime.timedelta(days=span)
-
+    timedelta = timenow - datetime.timedelta(days=span)
     timestamp = timedelta.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     if variables.key:
@@ -63,12 +67,26 @@ def main():
         try:
             apikey = (os.environ["MERAKI_APIKEY"])
         except:
-            print("API Key missing - Please set in Env variables or in Arguments")
             sys.exit()
+    try:
+        s = session(apikey)
+    except meraki.APIError:
+        print("API Error - check arguments")
+        return
+    except meraki.APIKeyError:
+        print("API Key Error")
+        return
 
     if not org:
         # Display all organizations
-        organ = getorganisations(apikey)
+        try :
+            organ = getorganisations(s)
+        except meraki.APIError:
+            print("API Error - check arguments")
+            return
+        except meraki.APIKeyError:
+            print("API Key Error")
+            return
         print("Please set Org ID in parameters - available organizations : ")
         for each in organ:
             # display available Org IDs
@@ -80,20 +98,34 @@ def main():
             # Parsing one network only
             product = prod
             event_type = type
-            event_count = getnbevents(apikey, net, product, event_type, timestamp)
-            print(f"Network : {net} , Number of {event_type} : {event_count}")
-
+            try :
+                event_count = getnbevents(s, net, product, event_type, timestamp)
+                print(f"Network : {net} , Number of {event_type} : {event_count}")
+            except meraki.APIError:
+                print("API Error - check arguments")
+                return
+            except meraki.APIKeyError:
+                print("API Key Error")
+                return
         else:
             # Parsing all networks if no network ID set
-            networks = getnetworks(apikey, org)
-            print(f"Timespan : between {timedelta} and {timenow}")
+            try:
+                networks = getnetworks(s, org)
+                print(f"Timespan : between {timedelta} and {timenow}")
+            except meraki.APIError:
+                print("API Error - check arguments")
+                return
+            except meraki.APIKeyError:
+                print("API Key Error")
+                return
             for each in networks:
                 netid = each.get('id')
                 netname = each.get('name')
                 product = prod
                 event_type = eventtype
-                event_count = getnbevents(apikey, netid, product, event_type, timestamp)
+                event_count = getnbevents(s, netid, product, event_type, timestamp)
                 print(f"Network : {netname} , Number of {event_type} : {event_count}")
 
 
 main()
+
